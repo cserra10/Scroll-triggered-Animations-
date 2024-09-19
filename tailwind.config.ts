0009dd53ab6f23290dd786cabb1911a3cbd38636
Cesar Serrano <cesr90@gmail.com>
2024-09-19 03:25:59 -0500

    Scroll-triggered Animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,17 @@
 import type { Config } from "tailwindcss";
 
+const opacityArray = Array(100).fill(0).map((_, i) => `opacity-[${i}%]`);
+const translateXArray = Array(100).fill(0).map((_, i) => `translate-x-[${i}%]`);
+const scaleArray = Array(200).fill(0).map((_, i) => `scale-[${i}%]`);
+const rotateArray = Array(360).fill(0).map((_, i) => `rotate-[${i}deg]`);
+
 const config: Config = {
+  safelist: [
+    ...opacityArray,
+    ...translateXArray,
+    ...scaleArray,
+    ...rotateArray
+  ],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,6 +25,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
